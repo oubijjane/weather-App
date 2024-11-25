42c4a43281e0128102431b4fb3a0d4228c3a7168
oubijjane <79411385+oubijjane@users.noreply.github.com>
2024-11-25 14:01:10 +0100

    . make the initial layout for the website .get the weather for the next 15 days an dispalay theme

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ async function getData(location) {
     const data = await response.json();
 
     const temp = await data.days[0].temp;
+    const date = await data.days[0].datetime;
     const condition = await data.days[0].conditions;
     const minTemp = await data.days[0].tempmin;
     const maxTemp = await data.days[0].tempmax;
@@ -20,9 +21,9 @@ async function getData(location) {
     const windDirection = await data.days[0].winddir;
     const endTime = Date.now();
     const timeTaken = endTime - startTime;
-    finishLoading();
     cityWeather = weather(
       location,
+      date,
       temp,
       condition,
       minTemp,
@@ -31,15 +32,20 @@ async function getData(location) {
       windSpeed,
       windDirection
     );
+    displayData(cityWeather);
+    finishLoading();
+
     console.log(`total time ${timeTaken}ms`);
     console.log(cityWeather.info());
   } catch (e) {
     console.log("error: " + e);
+    finishLoading();
   }
 }
 
 function weather(
   location,
+  date,
   temp,
   condition,
   minTemp,
@@ -63,13 +69,15 @@ function weather(
   const getWindSpeed = () => windSpeed;
 
   const getWindDirection = () => windDirection;
+  const getDate = () => date;
 
   const info = () =>
-    `address: ${location}, temp: ${temp}c°, condition: ${condition}, Min: ${minTemp}c°, Max: ${maxTemp}c°,` +
+    `address: ${location}, date: ${date}, temp: ${temp}c°, condition: ${condition}, Min: ${minTemp}c°, Max: ${maxTemp}c°,` +
     `humidity: ${humidity}% wind speed: ${windSpeed}kph, wind direction: ${windDirection}°  `;
 
   return {
     getLocation,
+    getDate,
     getTemp,
     getCondition,
     getMinTemp,
@@ -91,21 +99,100 @@ function eventBtn() {
   const btn = document.querySelector(".search");
   const cityInput = document.querySelector("#city");
   btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("hello");
+    const div = document.querySelector(".container");
+    const cards = document.querySelector(".cards");
+    div.replaceChildren();
+    cards.replaceChildren();
     getData(cityInput.value);
+    getAllData(cityInput.value);
   });
 }
-function Startlaoding() {
+function Startlaoding(timout) {
   const btn = document.querySelector(".search");
   btn.disabled = true;
-
-  btn.textContent = "loading";
 }
 function finishLoading() {
   const btn = document.querySelector(".search");
   btn.disabled = false;
   btn.textContent = "search";
 }
+function displayData(cityWeather) {
+  const div = document.querySelector(".container");
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
+  const p4 = document.createElement("p");
+  const p5 = document.createElement("p");
+  const p6 = document.createElement("p");
+  const p7 = document.createElement("p");
+  const p8 = document.createElement("p");
+  const p9 = document.createElement("p");
 
+  p1.textContent = "location: " + cityWeather.getLocation();
+  p2.textContent = "Temp: " + cityWeather.getTemp();
+  p3.textContent = "condition: " + cityWeather.getCondition();
+  p4.textContent = "min temp: " + cityWeather.getMinTemp();
+  p5.textContent = "max temp: " + cityWeather.getMaxTemp();
+  p6.textContent = "Humidity: " + cityWeather.getHumidity();
+  p7.textContent = "wind speed: " + cityWeather.getWindSpeed();
+  p8.textContent = "wind direction: " + cityWeather.getWindDirection();
+  p9.textContent = "date: " + cityWeather.getDate();
+
+  div.append(p1, p9, p2, p3, p4, p5, p6, p7, p8);
+}
+function card(cityWeather) {
+  const div = document.querySelector(".cards");
+  const card = document.createElement("div");
+  card.className = "card";
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
+  const p4 = document.createElement("p");
+  p1.textContent = "condition: " + cityWeather.getCondition();
+  p2.textContent = "date: " + cityWeather.getDate();
+  p3.textContent = "min temp: " + cityWeather.getMinTemp();
+  p4.textContent = "max temp: " + cityWeather.getMaxTemp();
+
+  card.append(p1, p2, p3, p4);
+
+  div.appendChild(card);
+}
+
+async function getAllData(location) {
+  let cityWeather = "";
+  Startlaoding();
+  const startTime = Date.now();
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=8LFCZ2WZPLTSWFHLPXM5PHWG6&unitGroup=metric`,
+      { mode: "cors" }
+    );
+    const data = await response.json();
+
+    const days = await data.days;
+    const endTime = Date.now();
+    const timeTaken = endTime - startTime;
+    days.forEach((day) => {
+      cityWeather = weather(
+        location,
+        day.datetime,
+        day.temp,
+        day.conditions,
+        day.tempmin,
+        day.tempmax,
+        day.humidity,
+        day.windspeed,
+        day.winddir
+      );
+      card(cityWeather);
+    });
+    finishLoading();
+
+    console.log(`total time ${timeTaken}ms`);
+    console.log(cityWeather.info());
+  } catch (e) {
+    console.log("error: " + e);
+    finishLoading();
+  }
+}
 eventBtn();
